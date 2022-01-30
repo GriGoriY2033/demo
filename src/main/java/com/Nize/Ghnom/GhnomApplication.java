55e0a80d5313863cf67c3482891fcc0d65181b57
user <grigor_11_11@mail.ru>
2022-01-30 23:32:04 +0300

    Initial commit

---
+++ src/main/java/com/Nize/Ghnom/GhnomApplication.java
@@ -0,0 +1,13 @@
+package com.Nize.Ghnom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhnomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhnomApplication.class, args);
+	}
+
+}
